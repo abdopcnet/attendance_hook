5253ee432b841bf3797f8b608cda540389306a6d
abdopcnet <abdopcnet@gmail.com>
2025-12-18 06:26:04 +0200

    ❌ tax_childs.py

--- attendance_hook/attendance_hook/doctype/tax_childs/tax_childs.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Mohammed Nasser and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class TaxChilds(Document):
-	pass

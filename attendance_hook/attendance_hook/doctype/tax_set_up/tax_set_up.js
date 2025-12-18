d139ebe18e6f0088840a5ebd291263942f056fcb
abdopcnet <abdopcnet@gmail.com>
2025-12-18 06:26:04 +0200

    ❌ tax_set_up.js

--- attendance_hook/attendance_hook/doctype/tax_set_up/tax_set_up.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Mohammed Nasser and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Tax Set-Up", {
-// 	refresh(frm) {
-
-// 	},
-// });
